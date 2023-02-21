3f8cda0418a0a399c0d4fb0d546d2ce1630f4c78
dmitriy.kusch <dmitriy.kusch@gmail.com>
2023-02-21 13:43:40 +0000

    added lib

---
+++ lib/door3-components-example-lib/door3-components-example-lib.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="door3-components-example-lib" />
+export * from './public-api';
---
+++ lib/door3-components-example-lib/lib/door3-components-example-lib.service.d.ts
@@ -0,0 +1,6 @@
+import * as i0 from "@angular/core";
+export declare class Door3ComponentsExampleLibService {
+    constructor();
+    static ɵfac: i0.ɵɵFactoryDeclaration<Door3ComponentsExampleLibService, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<Door3ComponentsExampleLibService>;
+}
